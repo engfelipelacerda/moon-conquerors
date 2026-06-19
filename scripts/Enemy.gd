a51622e6d9ed4260de8065e5cc50157ccfcf9bfe
Pedro Rickson <pedrorickson95.fa@gmail.com>
2026-06-19 15:32:48 -0300

    Feature: Improving-hovering

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,54 +1,34 @@
 extends CharacterBody2D
 
-@export var speed = 30;
+@export var speed = 80;
 @export var speed_rotation = 2;
-@export var stopping_distance = 280;
-@export var hovering_height = 65;
+@export var stopping_distance = 180;
+@export var hovering_height = 150;
 
 @export var fire_interval = 2
-@onready var player = $"../Player";
 var bullet_scene = preload("res://scenes/Bullet.tscn")
 var can_shoot := true;
 
-
 var angle = 0;
+
+@export var enemy_movement:EnemyMovement
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	add_to_group("lm_enemies")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if player == null:
-		return
-	var direction = (player.global_position - global_position).normalized()
-	hover(delta,direction)
-	if can_shoot:
-		shoot(direction)
-
-func hover(delta:float,direction:Vector2):
-	#Ground is at 570
-	var ground_distance = abs(global_position.y - 570)
-	var target_angle = rad_to_deg(direction.angle())
-	var distance = global_position.distance_to(player.global_position)
-	var target_velocity = direction * speed
+func _physics_process(delta: float) -> void:
+	var steering_force = enemy_movement.move_vector()
+	var movement = steering_force* speed
+	velocity = velocity.lerp(movement, 0.8)
 	
-	if target_angle < 90:
-		angle = deg_to_rad(45)
-	elif target_angle > 90:
-		angle = deg_to_rad(-45)
-	
-	if distance < stopping_distance:
-		target_velocity = direction * 0
-		angle = 0
-	
-	velocity = velocity.lerp(target_velocity, 0.1)
-
-	if ground_distance < hovering_height:
-		velocity.y = 0
+	var angle = enemy_movement.hover_angle(steering_force)
+	rotation = lerp_angle(rotation,angle,speed_rotation*delta)
 	
 	move_and_slide()
-	rotation = lerp_angle(rotation,angle,speed_rotation*delta)
-
+	if can_shoot:
+		pass
+	
 func shoot(direction):
 	can_shoot = false;
 	var bullet = bullet_scene.instantiate()
@@ -58,5 +38,3 @@ func shoot(direction):
 	bullet.direction = direction;
 	await get_tree().create_timer(fire_interval*randf()).timeout
 	can_shoot = true       
-
-	
---
+++ scripts/enemy_movement.gd
@@ -0,0 +1,80 @@
+extends Node2D
+class_name EnemyMovement
+
+var player:CharacterBody2D = null;
+var ground_raycast:RayCast2D = null;
+var floor_repelling_force:Vector2 = Vector2.ZERO;
+var steering_force = Vector2.ZERO
+
+
+var MAX_RANGE_GROUND_RAYCAST = 900
+var DEFAULT_STOPPING_DISTANCE = 300
+var DEFAULT_SEPARATION_STRENGTH = 2.2
+var DEFAULT_HOVERING_HEIGHT = 250
+
+
+@export var stopping_distance:float = DEFAULT_STOPPING_DISTANCE
+@export var separation_force_strength = DEFAULT_SEPARATION_STRENGTH
+@export var hovering_height = DEFAULT_HOVERING_HEIGHT
+
+func _ready() -> void:
+	player = Game.player
+	ground_raycast = $"../RayCast2D";
+	ground_raycast.target_position = Vector2(0,MAX_RANGE_GROUND_RAYCAST)
+
+func move_vector() -> Vector2:
+	floor_repelling_force = Vector2.ZERO
+	steering_force = Vector2.ZERO
+	var enemy_to_player = (global_position - player.global_position)
+	
+	var player_force = Vector2.ZERO
+	if enemy_to_player.length() > stopping_distance:
+		player_force = -enemy_to_player.normalized()
+	elif enemy_to_player.length() < stopping_distance - 100:
+		player_force = enemy_to_player.normalized()
+	else:
+		player_force = Vector2.ZERO
+	
+	#Boids System
+	var separation_force:Vector2 = Vector2.ZERO
+	for neighbor:CharacterBody2D in get_all_neighbors():
+		var enemy_to_neighbor = global_position - neighbor.global_position
+		var neighbor_distance = enemy_to_neighbor.length()
+		var neighbor_direction = enemy_to_neighbor.normalized()
+		
+		var strength = (stopping_distance - neighbor_distance)/ stopping_distance
+		
+		if neighbor_distance < stopping_distance:
+			separation_force+=neighbor_direction*strength;
+		elif neighbor_distance > (stopping_distance + 50):
+			separation_force = Vector2.ZERO
+	
+	steering_force+=separation_force
+	
+	#Minimum Hovering height
+	var ground_distance = get_ground_distance()
+	if ground_distance < hovering_height:
+		var strength = (hovering_height - ground_distance) / hovering_height
+		floor_repelling_force = Vector2.UP*strength
+	elif ground_distance > (hovering_height + 100):
+		floor_repelling_force = Vector2.ZERO
+	
+	steering_force = (
+		player_force
+		+floor_repelling_force * 4.0
+		+separation_force * 2.0
+		).normalized()
+	
+	return steering_force
+	
+func get_all_neighbors() -> Array:
+	var neighbors = get_tree().get_nodes_in_group("lm_enemies")
+	neighbors.erase(self)
+	return neighbors
+
+func get_ground_distance() -> float:
+	var ground_distance = ground_raycast.get_collision_point().distance_to(global_position)
+	return ground_distance
+
+func hover_angle(steering_force:Vector2) -> float:
+	return deg_to_rad(25.0 * steering_force.x)
