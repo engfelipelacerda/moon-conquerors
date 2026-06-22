59a561dc4bdd6e3c21989d3a281c6aa2b2fdab80
Luis Felipe Pessoa Lacerda <engluisfelipepessoa@gmail.com>
2026-06-22 00:18:34 -0300

    adicionando animacao explosao

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,22 +1,18 @@
 extends CharacterBody2D
-
 # Configurações de movimentação
 @export var speed = 80
 @export var speed_rotation = 2
 @export var stopping_distance = 180
 @export var hovering_height = 150
-
 # Configurações de ataque
 @export var fire_interval = 2
 @export var enemy_damage = 30
 @export var shooting_distance = 500
-
 var bullet_scene = preload("res://entities/EnemyBullet.tscn")
 @onready var animation = $AnimatedSprite2D
-
 var can_shoot := true
 var angle = 0
-
+var is_dying := false
 # Estratégia de movimentação do inimigo
 @export var enemy_movement: EnemyMovement
 # Saúde do inimigo
@@ -35,43 +31,38 @@ func setup(config:EnemyConfig):
 func _ready() -> void:
 	# Adiciona o inimigo ao grupo de inimigos
 	add_to_group("lm_enemies")
+	enemy_health.died.connect(_on_died)
 
 func _physics_process(delta: float) -> void:
+	if is_dying:
+		return
+
 	if Game.player == null:
 		return
 	
 	# Toca a animação
-	animation.play()
+	animation.play("flying")
 	
 	# Calcula a direção desejada de movimento
 	var steering_force = enemy_movement.move_vector()
 	var movement = steering_force * speed
-
 	# Suaviza a movimentação
 	velocity = velocity.lerp(movement, 0.8)
-
 	# Rotaciona o inimigo na direção do movimento
 	var angle = enemy_movement.hover_angle(steering_force)
 	rotation = lerp_angle(rotation, angle, speed_rotation * delta)
-
 	move_and_slide()
-
 	if can_shoot and Game.player.global_position.distance_to(global_position) < shooting_distance :
 		shoot(Game.player)
-
 	
 func shoot(target):
 	var direction = (target.global_position - global_position).normalized()
-
 	# Impede disparos contínuos
 	can_shoot = false
-
 	# Cria e posiciona o projétil
 	var bullet = bullet_scene.instantiate()
 	bullet.global_position = global_position
-
 	get_parent().add_child(bullet)
-
 	# Configura bala
 	var config = BulletConfig.new()
 	config.faction_owner = Factions.Type.ENEMY
@@ -80,7 +71,14 @@ func shoot(target):
 	
 	# Define a direção do disparo
 	bullet.direction = direction
-
 	# Aguarda o tempo de recarga antes de permitir outro tiro
 	await get_tree().create_timer(fire_interval * randf()).timeout
 	can_shoot = true
+
+func _on_died():
+	is_dying = true
+	can_shoot = false
+	velocity = Vector2.ZERO
+	animation.play("kill")
+	await animation.animation_finished
+	queue_free()
--- scripts/character_health.gd
+++ scripts/character_health.gd
@@ -6,21 +6,27 @@ signal died
 @export var max_health = 100
 var health:float
 @export var is_player := false
+var is_dead := false
 
 func _ready() -> void:
 	health = max_health
 
 func damage(hit_value:float):
+	if is_dead:
+		return
 	health -= hit_value
-	print("Vida: ", health, " / ", max_health)
+	if is_player:
+		print("Vida do player: ", health, " / ", max_health)
 	if(health <= 0):
 		die()
 
 func die():
+	if is_dead:
+		return
+	is_dead = true
 	if is_player:
 		var game_over = preload("res://scenes/GameOver.tscn").instantiate()
 		get_tree().current_scene.add_child(game_over)
 		get_tree().paused = true
 	else:
 		died.emit()
-		get_parent().queue_free()
